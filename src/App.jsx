80b4bb35c03a8370e12720acbc6a40ec9c2a37c2
Enrico Barros <116385740+Enrico-Barros@users.noreply.github.com>
2023-09-23 00:01:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           <Route path="/Space-Tourism-Website" element={<Home />} />
           <Route path="/Destination" element={<Destination />} />
           <Route path="/Crew" element={<Crew />} />
-          <Route path="Technology" element={<Technology />} />
+          <Route path="/Technology" element={<Technology />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
